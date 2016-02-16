1e3692f584b39ca7c734c0e36422d25228464ed2
esmelusina <esmelusina@gmail.com>
2016-02-15 17:32:17 -0800

    asset manager fbos

--- GLInterface/Assets.h
+++ GLInterface/Assets.h
@@ -16,18 +16,21 @@ class Assets
 	std::unordered_map<std::string, Texture>	  textures;
 	std::unordered_map<std::string, RenderObject> renderObjects;
 	std::unordered_map<std::string, Shader>		  shader;
+	std::unordered_map<std::string, RenderTarget> renderTargets;
 public:
 	static Assets &instance() { static Assets a; return a; }
 
 	Texture      getTexture		 (const std::string &name);
 	RenderObject getRenderObject (const std::string &name);
 	Shader       getShader		 (const std::string &name);
+	RenderTarget getRenderTarget (const std::string &name);
 
 	// Just wrap cassetutils procedures, add a 'name' parameter
 	// so that we can name them memorable things, and drop them in our maps
 	void makeTexture	  (const char *name, unsigned w, unsigned h, unsigned depth, const unsigned char *pixels = nullptr);
 	void makeRenderObject (const char *name, const Vertex *verts, unsigned vsize, const unsigned *tris, unsigned tsize);
 	void makeShader		  (const char *name, const char *vsource, const char *fsource);
+	void makeRenderTarget (const char *name, unsigned w, unsigned h, const unsigned *depths, unsigned dcount );
 
 	void loadFBX	(const char *name, const char *path);
 	void loadOBJ	(const char *name, const char *path);
